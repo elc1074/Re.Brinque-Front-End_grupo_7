d82384eb93828035984c10d22932781b0453eae5
Breno Cortês Da Rosa <71451784+Akunozz@users.noreply.github.com>
2025-09-07 18:14:17 -0300

    add favicon diferent sizes

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,7 +11,43 @@ export default function manifest(): MetadataRoute.Manifest {
     background_color: "#316A41",
     theme_color: "#316A41",
     icons: [
-      { src: "/favicon.ico", sizes: "any", type: "image/x-icon" },
+      {
+        src: "/favicon.ico",
+        sizes: "any",
+        type: "image/x-icon",
+      },
+      {
+        src: "/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+      {
+        src: "/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+      {
+        src: "/web-app-manifest-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "maskable",
+      },
+      {
+        src: "/web-app-manifest-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
+      },
+      {
+        src: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
     ],
   };
 }
