efb8e04912430a81bdce03ad6f0294a310faaa6c
pedro-henrique-jv <147932093+pedro-henrique-jv@users.noreply.github.com>
2025-09-14 23:58:19 -0300

    Update route.ts

--- src/app/api/google-calback/route.ts
+++ src/app/api/google-calback/route.ts
@@ -4,5 +4,5 @@ export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const code = searchParams.get('code');
   
-  return NextResponse.redirect('https://front-re-brinque.vercel.app/google-sync');
+  return NextResponse.redirect('https://front-re-brinque.vercel.app/');
 }
